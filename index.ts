6a381f95e8575dd2e17c2d8da5c8f1518e2eaf95
kevinlul <6320810+kevinlul@users.noreply.github.com>
2022-10-30 11:35:33 -0400

    Replace main schema

--- index.ts
+++ index.ts
@@ -3,39 +3,81 @@ import { TSchema, Type } from "@sinclair/typebox";
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types,@typescript-eslint/explicit-function-return-type
 export const Nullable = <T extends TSchema>(type: T) => Type.Union([type, Type.Null()]);
 
-export const CardText = Type.Object({
-	name: Type.String(),
-	description: Type.String(),
-	pendulum: Type.Optional(Type.String())
+export const LOCALES = [
+	"en", // English
+	"de", // German
+	"es", // Spanish
+	"fr", // French
+	"it", // Italian
+	"pt", // Portuguese
+	"ja", // Japanese
+	"ko", // Korean
+	"zh-CN", // Simplified Chinese
+	"zh-TW" // Traditional Chinese
+] as const;
+
+const locales = {
+	en: Nullable(Type.String()),
+	de: Nullable(Type.String()),
+	es: Nullable(Type.String()),
+	fr: Nullable(Type.String()),
+	it: Nullable(Type.String()),
+	pt: Nullable(Type.String()),
+	ja: Nullable(Type.String()),
+	ko: Nullable(Type.String()),
+	"zh-CN": Nullable(Type.String()),
+	"zh-TW": Nullable(Type.String())
+};
+
+export const CardSet = Type.Object({
+	set_number: Type.String(),
+	set_name: Type.String(),
+	// could be enum but who knows
+	rarities: Nullable(Type.Array(Type.String()))
 });
 
+export const CardSetList = Type.Optional(Type.Array(CardSet));
+
+export enum LimitRegulation {
+	Forbidden = "Forbidden",
+	Limited = "Limited",
+	SemiLimited = "Semi-Limited",
+	Unlimited = "Unlimited",
+	Unreleased = "Not yet released",
+	// For most purposes equivalent to the above
+	NotYetLegal = "Not yet legal"
+}
+
 const base = {
-	password: Type.Integer(),
-	kid: Nullable(Type.Integer()), // constrain minimum in the future when data not bugged
-
-	// TODO: make on-demand if we get performance issues
-	en: CardText,
-	fr: Type.Optional(CardText),
-	de: Type.Optional(CardText),
-	it: Type.Optional(CardText),
-	pt: Type.Optional(CardText),
-	// es, ja, ko, zh
-
-	sets: Type.Array(
-		Type.Object({
-			// These shouldn't be optional but the database often has mismatches in its raw delimited form
-			tag: Type.Optional(Type.String()), // Card Number
-			name: Type.Optional(Type.String())
-		})
+	konami_id: Nullable(Type.Integer({ minimum: 0 })),
+	password: Nullable(Type.Integer({ minimum: 0 })),
+	fake_password: Type.Optional(
+		Type.Union([Type.Integer({ minimum: 0 }), Type.Array(Type.Integer({ minimum: 0 }), { minItems: 1 })])
 	),
-
-	// extra temporaries
-	format: Nullable(Type.String()),
-	archetypes: Nullable(Type.String()),
-	releaseTCG: Nullable(Type.String()),
-	releaseOCG: Nullable(Type.String()),
-	statusTCG: Nullable(Type.String()),
-	statusOCG: Nullable(Type.String())
+	name: Type.Object({
+		...locales,
+		ja_romaji: Nullable(Type.String()),
+		ko_rr: Nullable(Type.String())
+	}),
+	text: Type.Object(locales),
+	sets: Type.Object({
+		en: CardSetList,
+		de: CardSetList,
+		es: CardSetList,
+		fr: CardSetList,
+		it: CardSetList,
+		pt: CardSetList,
+		ja: CardSetList,
+		ko: CardSetList,
+		"zh-CN": CardSetList,
+		"zh-TW": CardSetList
+	}),
+	limit_regulation: Type.Object({
+		// null = Not yet released, structure may be changed in the future
+		tcg: Nullable(Type.Enum(LimitRegulation)),
+		ocg: Nullable(Type.Enum(LimitRegulation)),
+		speed: Type.Optional(Type.Enum(LimitRegulation))
+	})
 };
 
 export enum Attribute {
@@ -48,83 +90,25 @@ export enum Attribute {
 	DIVINE = "DIVINE"
 }
 
-export enum Race {
-	Aqua = "Aqua",
-	Beast = "Beast",
-	BeastWarrior = "Beast-Warrior",
-	CreatorGod = "Creator-God",
-	Cyberse = "Cyberse",
-	Dinosaur = "Dinosaur",
-	DivineBeast = "Divine-Beast",
-	Dragon = "Dragon",
-	Fairy = "Fairy",
-	Fiend = "Fiend",
-	Fish = "Fish",
-	Insect = "Insect",
-	Machine = "Machine",
-	Plant = "Plant",
-	Psychic = "Psychic",
-	Pyro = "Pyro",
-	Reptile = "Reptile",
-	Rock = "Rock",
-	SeaSerpent = "Sea Serpent",
-	Spellcaster = "Spellcaster",
-	Thunder = "Thunder",
-	Warrior = "Warrior",
-	WingedBeast = "Winged Beast",
-	Wyrm = "Wyrm",
-	Zombie = "Zombie"
-}
-
-// This name is not official and is only used in the OCG
-enum Ability {
-	// Not actually abilities, but we have too many "types" going on that all appear on the same line
-	Normal = "Normal",
-	Effect = "Effect",
-	Tuner = "Tuner",
-	// Not even a card
-	Token = "Token",
-	// Special Summon
-	// Actual abilities, which seem to be mutually exclusive
-	Flip = "Flip",
-	Toon = "Toon",
-	Spirit = "Spirit",
-	Union = "Union",
-	Gemini = "Gemini"
-}
+export const MonsterPoints = Type.Union([Type.Integer({ minimum: 0 }), Type.Literal("?")]);
 
 const baseMonster = {
 	...base,
-	type: Type.Literal("Monster"),
-	// Meant to an array of the literal type line on the card split on slashes because
-	// it's so confusing. Currently the YGOPRODECK-provided custom value.
-	typeline: Type.String(),
+	card_type: Type.Literal("Monster"),
+	monster_type_line: Type.String(),
 	attribute: Type.Enum(Attribute),
-	race: Type.Enum(Race),
-	// TODO: extend to -1 for ?
-	atk: Type.Integer({ minimum: 0 }),
-	// always false for Fusions due to a data bug
-	tuner: Type.Boolean(),
-	// Presence indicates Pendulum property, too annoying to do a schema with it mixed in all the types
-	scale: Type.Optional(Type.Integer({ minimum: 0, maximum: 13 }))
+	atk: MonsterPoints,
+	// both or neither
+	pendulum_scale: Type.Optional(Type.Integer({ minimum: 0, maximum: 13 })),
+	pendulum_effect: Type.Optional(Type.Object(locales))
 };
 
 const withLevel = {
-	// refers to the printed number of stars, regardless of gameplay (Ultimaya)
+	// refers to the printed number of stars, regardless of gameplay (e.g. Ultimaya)
 	level: Type.Integer({ minimum: 0, maximum: 12 }),
-	// TODO: extend to -1 for ?
-	def: Type.Integer({ minimum: 0 })
+	def: MonsterPoints
 };
 
-const xyz = {
-	subtype: Type.Literal("Xyz"),
-	// refers to the printed number of stars, regardless of gameplay (F0, iC1000)
-	rank: Type.Integer({ minimum: 0, maximum: 13 }),
-	// TODO: extend to -1 for ?
-	def: Type.Integer({ minimum: 0 })
-};
-
-// The Unicode arrows that correspond to emoji
 export enum LinkArrow {
 	"Bottom-Left" = "↙",
 	Bottom = "⬇",
@@ -135,12 +119,6 @@ export enum LinkArrow {
 	Top = "⬆",
 	"Top-Right" = "↗"
 }
-
-const link = {
-	subtype: Type.Literal("Link"),
-	link: Type.Integer({ minimum: 1, maximum: 8 }),
-	arrows: Type.Array(Type.Enum(LinkArrow), { minItems: 1, maxItems: 8 }) // same number of elements as specified by `link`
-};
 
 export enum SpellType {
 	Normal = "Normal",
@@ -158,59 +136,40 @@ export enum TrapType {
 	Counter = "Counter"
 }
 
-const NormalMonsterCardSchema = Type.Object({
+// Normal, Orange Effect, Ritual
+const MainDeckMonsterWithLevelCardSchema = Type.Object({
 	...baseMonster,
 	...withLevel,
-	effect: Type.Literal(false),
-	subtype: Type.Literal("Normal")
+	ritual_spell: Type.Optional(Type.String())
 });
-const TokenCardSchema = Type.Object({
+// Fusion, Synchro
+const ExtraDeckMonsterWithLevelCardSchema = Type.Object({
 	...baseMonster,
 	...withLevel,
-	effect: Type.Literal(false),
-	subtype: Type.Literal("Token")
+	materials: Type.String()
 });
-const OrangeMonsterCardSchema = Type.Object({
+const XyzMonsterCardSchema = Type.Object({
 	...baseMonster,
-	...withLevel,
-	effect: Type.Literal(true),
-	subtype: Type.Null()
+	// refers to the printed number of stars, regardless of gameplay (e.g. F0, iC1000)
+	rank: Type.Integer({ minimum: 0, maximum: 13 }),
+	def: MonsterPoints
 });
-const RitualMonsterCardSchema = Type.Object({
+const LinkMonsterCardSchema = Type.Object({
 	...baseMonster,
-	...withLevel,
-	effect: Type.Boolean(),
-	subtype: Type.Literal("Ritual")
+	// elements are unique
+	link_arrows: Type.Array(Type.Enum(LinkArrow), { minItems: 1, maxItems: 8 })
 });
-const FusionMonsterCardSchema = Type.Object({
-	...baseMonster,
-	...withLevel,
-	effect: Type.Boolean(),
-	subtype: Type.Literal("Fusion")
-});
-const SynchroMonsterCardSchema = Type.Object({
-	...baseMonster,
-	...withLevel,
-	effect: Type.Boolean(),
-	subtype: Type.Literal("Synchro")
-});
-const XyzMonsterCardSchema = Type.Object({ ...baseMonster, effect: Type.Boolean(), ...xyz });
-const LinkMonsterCardSchema = Type.Object({ ...baseMonster, effect: Type.Boolean(), ...link });
-const SpellCardSchema = Type.Object({ ...base, type: Type.Literal("Spell"), subtype: Type.Enum(SpellType) });
-const TrapCardSchema = Type.Object({ ...base, type: Type.Literal("Trap"), subtype: Type.Enum(TrapType) });
+const SpellCardSchema = Type.Object({ ...base, card_type: Type.Literal("Spell"), property: Type.Enum(SpellType) });
+const TrapCardSchema = Type.Object({ ...base, card_type: Type.Literal("Trap"), property: Type.Enum(TrapType) });
 
 export const CardSchema = Type.Union(
 	[
-		NormalMonsterCardSchema,
-		TokenCardSchema,
-		OrangeMonsterCardSchema,
-		RitualMonsterCardSchema,
-		FusionMonsterCardSchema,
-		SynchroMonsterCardSchema,
+		MainDeckMonsterWithLevelCardSchema,
+		ExtraDeckMonsterWithLevelCardSchema,
 		XyzMonsterCardSchema,
 		LinkMonsterCardSchema,
 		SpellCardSchema,
 		TrapCardSchema
 	],
-	{ $id: "https://api.alphakretin.com/card.json" }
+	{ $id: "https://api.alphakretin.com/yaml-yugi/card.json" }
 );
